09aadefef758a0fa15b6158118059bbaa43e567c
Hede <Heatherj.swift@gmail.com>
2021-03-28 18:09:24 -0400

    5 questions work and are picked in a random order

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,13 +48,18 @@ function resetState() {
 }
 
 function selectAnswer(e) {
-    const selectButton = e.target
-    const correct = selectButton.dataset.correct
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
     setStatusClass(document.body, correct)
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }   
 }
 
 function setStatusClass(element, correct) {
@@ -81,7 +86,48 @@ const questions = [
             { text: 'helloWorld()', correct: true},
             { text: 'alert("Hello World")', correct: false}
         ]
+    },
+    {
+        question: "number values in JavaScript can't be...",
+        answers: [
+            { text: 'decimals', correct: false},
+            { text: 'negative', correct: false},
+            { text: 'integers', correct: false},
+            { text: 'letters', correct: true}
+        ]
+    },
+    {
+        question: "A boolean value can only have 1 of 2 values. They are ...",
+        answers: [
+            { text: 'correct or incorrect', correct: false},
+            { text: 'true or false', correct: true},
+            { text: 'T or F', correct: false},
+            { text: 'right or wrong', correct: false}
+        ]
+    },
+    {
+        question: "A string in javaScript is ...",
+        answers: [
+            { text: 'A collection of characters', correct: true},
+            { text: 'A collection of numbers', correct: false},
+            { text: 'Any collection of values', correct: false},
+            { text: 'by default a randomized bunch of letters', correct: false}
+        ]
+    },
+    {
+        question: "Pick the option that ISN'T a functional array. Pay close attention to the syntax.",
+        answers: [
+            { text: '["apples","oranges","tomatoes"]', correct: false},
+            { text: '[1,2,3,7,8,9,22]', correct: false},
+            { text: '[true, false]', correct: true},
+            { text: '["cat","dog","mouse"]', correct: false}
+        ]
     }
+
 ]
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
