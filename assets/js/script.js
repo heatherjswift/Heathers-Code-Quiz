da90246ac68583f6c6057688dd55b0fb64fd1b93
Hede <Heatherj.swift@gmail.com>
2021-03-28 14:42:38 -0400

    start button and starting instructions disappear when button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
 // start with a score of 0
 var score = 0;
 
+let startButton = document.getElementById("start-btn")
+let startPage = document.getElementsByClassName("start-page")
+
+console.log()
+
+function startGame() {
+    console.log("Started")
+    startButton.classList.add('hide')
+    startPage[0].classList.add('hide')
+    startPage[1].classList.add('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion () {
+
+}
+
+function selectAnswer() {
+
+}
+
+startButton.addEventListener('click', startGame)
