eed5f0ab65177714ea384b35f7eaf9997836ce7c
Hede <Heatherj.swift@gmail.com>
2021-03-28 16:05:52 -0400

    buttons show wrong or right when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,87 @@
 // start with a score of 0
 var score = 0;
 
-let startButton = document.getElementById("start-btn")
-let startPage = document.getElementsByClassName("start-page")
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const startPage = document.getElementsByClassName('start-page')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-console.log()
+let shuffledQuestions, currentQuestionIndex
 
 function startGame() {
-    console.log("Started")
     startButton.classList.add('hide')
     startPage[0].classList.add('hide')
     startPage[1].classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
-function setNextQuestion () {
-
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
-
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectButton = e.target
+    const correct = selectButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    nextButton.classList.remove('hide')
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+
+const questions = [
+    {
+        question: "Pick the option that ISN'T a way to see JavaScript results.",
+        answers: [
+            { text: 'console.log("Hello World!");', correct: false},
+            { text: 'document.write("Hello World!);', correct: false},
+            { text: 'helloWorld()', correct: true},
+            { text: 'alert("Hello World")', correct: false}
+        ]
+    }
+]
 
 startButton.addEventListener('click', startGame)
